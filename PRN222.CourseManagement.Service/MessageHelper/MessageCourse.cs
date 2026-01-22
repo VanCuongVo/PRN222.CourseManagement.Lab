9428fc12afc78f1d77cd7f91b50b4f0cde2a2ca1
VoVanCuong <cuongcon2k04@gmail.com>
2026-01-22 23:08:22 +0700

    [Add]Implemetation feature course

---
+++ PRN222.CourseManagement.Service/MessageHelper/MessageCourse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN222.CourseManagement.Service.MessageHelper
+{
+    public interface MessageCourse
+    {
+        // ===== SUCCESS =====
+        public const string COURSE_CREATE_SUCCESS =
+            "Course created successfully.";
+
+        public const string COURSE_UPDATE_SUCCESS =
+            "Course updated successfully.";
+
+        public const string COURSE_DELETE_SUCCESS =
+            "Course deleted successfully.";
+
+        // ===== NOT FOUND =====
+        public const string COURSE_NOT_FOUND =
+            "Course not found.";
+
+        // ===== BUSINESS RULE ERRORS =====
+
+        // BR11: CourseCode must be unique
+        public const string COURSE_CODE_DUPLICATED =
+            "Course code must be unique.";
+
+        // BR12: Course must belong to exactly one department
+        public const string COURSE_DEPARTMENT_REQUIRED =
+            "Course must belong to exactly one department.";
+
+        // BR13: Course credits must be between 1 and 6
+        public const string COURSE_CREDITS_INVALID =
+            "Course credits must be between 1 and 6.";
+
+        // BR14: A course cannot be deleted if students are enrolled
+        public const string COURSE_HAS_ENROLLMENTS =
+            "Cannot delete course because students are enrolled.";
+
+        // BR15: A course cannot be updated if it is inactive or archived
+        public const string COURSE_INACTIVE_OR_ARCHIVED =
+            "Cannot update course because it is inactive or archived.";
+    }
+}
--- PRN222.CourseManagement.Service/Service/CourseService.cs
+++ PRN222.CourseManagement.Service/Service/CourseService.cs
@@ -1,7 +1,183 @@
-﻿namespace PRN222.CourseManagement.Service.Service
-{
-    public class CourseService
-    {
+﻿using PRN222.CourseManagement.Repository.IUnitOfWork;
+using PRN222.CourseManagement.Repository.Models;
+using PRN222.CourseManagement.Service.DTO.Request;
+using PRN222.CourseManagement.Service.IService;
+using PRN222.CourseManagement.Service.MessageHelper;
 
+namespace PRN222.CourseManagement.Service.Service
+{
+    public class CourseService : ICourseService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CourseService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public ServiceResult Create(Course entity)
+        {
+            var result = new ServiceResult();   
+            try
+            {
+                var validateResult = ValidateCourseForCreate(entity);
+                if (!validateResult.IsSuccess)
+                    return validateResult;
+
+                _unitOfWork.courseRepository.Add(entity);
+                _unitOfWork.SaveChangeAsync();
+
+                result.IsSuccess = true;
+                result.Message = MessageCourse.COURSE_CREATE_SUCCESS;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+        private ServiceResult ValidateCourseForCreate(Course entity)
+        {
+            var result = new ServiceResult();
+            if (_unitOfWork.courseRepository
+     .Exists(c => c.CourseCode == entity.CourseCode))
+            {
+                result.IsSuccess = false;
+                result.Message = MessageHelper.MessageCourse.COURSE_CODE_DUPLICATED;
+
+            }
+            // BR12: Department exists
+            if (!_unitOfWork.departmentRepository
+                .Exists(d => d.DepartmentId == entity.DepartmentId))
+            {
+                result.IsSuccess = false;
+                result.Message = MessageHelper.MessageCourse.COURSE_DEPARTMENT_REQUIRED;
+            }
+
+            // BR13: Credits between 1 and 6
+            if (entity.Credits < 1 || entity.Credits > 6)
+            {
+                result.IsSuccess = false;
+                result.Message = MessageHelper.MessageCourse.COURSE_CREDITS_INVALID;
+            }
+
+            return result;
+        }
+
+        public ServiceResult Delete(int id)
+        {
+            var result = new ServiceResult();
+            try
+            {
+                var validateResult = ValidateCourseForDelete(id);
+                if (!validateResult.IsSuccess)
+                    return validateResult;
+
+                _unitOfWork.courseRepository.Delete(id);
+                _unitOfWork.SaveChangeAsync();
+                result.IsSuccess = true;
+                result.Message = MessageCourse.COURSE_DELETE_SUCCESS;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+
+        private ServiceResult ValidateCourseForDelete(int courseId)
+        {
+            var result = new ServiceResult();
+            // BR14: Cannot delete if students enrolled
+            bool hasEnrollment = _unitOfWork.enrollementRepository
+                .Exists(e => e.CourseId == courseId);
+
+            if (hasEnrollment)
+            {
+                result.IsSuccess = true;
+                result.Message = MessageHelper.MessageCourse.COURSE_HAS_ENROLLMENTS;
+            }
+
+            return result;
+        }
+
+        public IEnumerable<Course> GetAll()
+        {
+            return _unitOfWork.courseRepository.GetAll();
+        }
+
+        public ServiceResult Update(Course entity)
+        {
+            var result = new ServiceResult();
+            try
+            {
+                var validateResult = ValidateCourseForUpdate(entity);
+                if (!validateResult.IsSuccess)
+                    return validateResult;
+
+                var course = _unitOfWork.courseRepository
+                    .GetById(entity.CourseId);
+
+                if (course == null)
+                {
+                    result.IsSuccess = false;
+                    result.Message = MessageHelper.MessageCourse.COURSE_NOT_FOUND;
+                }
+
+                // Update fields
+                course.Credits = entity.Credits;
+                course.DepartmentId = entity.DepartmentId;
+
+                _unitOfWork.courseRepository.Update(course);
+                _unitOfWork.SaveChangeAsync();
+
+                result.IsSuccess = true;
+                result.Message = MessageHelper.MessageCourse.COURSE_UPDATE_SUCCESS;
+            }
+            catch (Exception ex)
+            {
+                result.IsSuccess = false;
+                result.Message = ex.Message;
+            }
+            return result;
+        }
+
+        private ServiceResult ValidateCourseForUpdate(Course entity)
+        {
+            var result = new ServiceResult();
+            // BR11: CourseCode unique (exclude itself)
+            bool duplicatedCode = _unitOfWork.courseRepository
+                .Exists(c => c.CourseId != entity.CourseId);
+
+            if (duplicatedCode)
+            {
+                result.IsSuccess = false;
+                result.Message = MessageHelper.MessageCourse.COURSE_NOT_FOUND;
+            }
+
+            // BR12: Department exists
+            if (!_unitOfWork.departmentRepository
+                .Exists(d => d.DepartmentId == entity.DepartmentId))
+            {
+                result.IsSuccess = false;
+                result.Message = MessageHelper.MessageCourse.COURSE_DEPARTMENT_REQUIRED;
+            }
+
+            // BR13: Credits between 1 and 6
+            if (entity.Credits < 1 || entity.Credits > 6)
+            {
+                result.IsSuccess = false;
+                result.Message = MessageHelper.MessageCourse.COURSE_CREDITS_INVALID;
+            }
+
+            return result;
+
+        }
     }
 }
