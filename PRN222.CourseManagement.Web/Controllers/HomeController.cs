84cffe2ca3586438bc2a3eb0e85553303a365f7e
VoVanCuong <cuongcon2k04@gmail.com>
2026-03-07 11:31:24 +0700

    [Uodate]

--- PRN222.CourseManagement.Web/Controllers/HomeController.cs
+++ PRN222.CourseManagement.Web/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PRN222.CourseManagement.Web.Models;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace PRN222.CourseManagement.Web.Controllers
 {
+    [ExcludeFromCodeCoverage]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
